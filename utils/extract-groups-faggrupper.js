3492a7239e95e4df871696d1d7b0debd9616f5d6
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-15 07:28:50 +0200

    Fixes repacking of group

--- utils/extract-groups-faggrupper.js
+++ utils/extract-groups-faggrupper.js
@@ -2,10 +2,11 @@
   const { writeFile } = require('fs').promises
   const logger = require('../lib/logger')
   const isFaggruppe = require('../lib/is-faggruppe')
+  const repackGroup = require('../lib/repack-group')
   const groups = require('../data/groups.json')
   logger('info', ['utils', 'extract-groups-faggrupper', 'groups', groups.length])
   const faggrupper = groups.filter(isFaggruppe)
   logger('info', ['utils', 'extract-groups-faggrupper', 'faggrupper', faggrupper.length])
-  await writeFile('data/faggrupper.json', JSON.stringify(faggrupper, null, 2), 'utf-8')
+  await writeFile('data/faggrupper.json', JSON.stringify(faggrupper.map(repackGroup), null, 2), 'utf-8')
   logger('info', ['utils', 'extract-groups-faggrupper', 'finished'])
 })()
