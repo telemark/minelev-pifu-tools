2fd1fb993be500c63cb8d8a978f9b05e790e2dae
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-15 08:22:46 +0200

    Implements repackPerson

--- utils/extract-persons-teachers.js
+++ utils/extract-persons-teachers.js
@@ -2,10 +2,11 @@
   const { writeFile } = require('fs').promises
   const isTeacher = require('../lib/is-teacher')
   const logger = require('../lib/logger')
+  const repackPerson = require('../lib/repack-person')
   const persons = require('../data/persons.json')
   logger('info', ['utils', 'extract-persons-teachers', 'persons', persons.length])
   const teachers = persons.filter(isTeacher)
   logger('info', ['utils', 'extract-persons-teachers', 'teachers', teachers.length])
-  await writeFile('data/teachers.json', JSON.stringify(teachers, null, 2), 'utf-8')
+  await writeFile('data/teachers.json', JSON.stringify(teachers.map(repackPerson), null, 2), 'utf-8')
   logger('info', ['utils', 'extract-persons-teachers', 'finished'])
 })()
