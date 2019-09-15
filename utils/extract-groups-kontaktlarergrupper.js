dc880d075cadb982e19e941962e7513aea2911a8
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-15 07:37:37 +0200

    Implements group repacking in extraction scripts

--- utils/extract-groups-kontaktlarergrupper.js
+++ utils/extract-groups-kontaktlarergrupper.js
@@ -2,10 +2,11 @@
   const { writeFile } = require('fs').promises
   const logger = require('../lib/logger')
   const isKontaktlarergruppe = require('../lib/is-kontaktlarergruppe')
+  const repackGroup = require('../lib/repack-group')
   const groups = require('../data/groups.json')
   logger('info', ['utils', 'extract-groups-kontaktlarergrupper', 'groups', groups.length])
   const kontaktlarergrupper = groups.filter(isKontaktlarergruppe)
   logger('info', ['utils', 'extract-groups-kontaktlarergrupper', 'kontaktlarergrupper', kontaktlarergrupper.length])
-  await writeFile('data/kontaktlarergrupper.json', JSON.stringify(kontaktlarergrupper, null, 2), 'utf-8')
+  await writeFile('data/kontaktlarergrupper.json', JSON.stringify(kontaktlarergrupper.map(repackGroup), null, 2), 'utf-8')
   logger('info', ['utils', 'extract-groups-kontaktlarergrupper', 'finished'])
 })()
--- utils/extract-groups-undervisningsgrupper.js
+++ utils/extract-groups-undervisningsgrupper.js
@@ -2,10 +2,11 @@
   const { writeFile } = require('fs').promises
   const logger = require('../lib/logger')
   const isUndervisningsgruppe = require('../lib/is-undervisningsgruppe')
+  const repackGroup = require('../lib/repack-group')
   const groups = require('../data/groups.json')
   logger('info', ['utils', 'extract-groups-undervisningsgrupper', 'groups', groups.length])
   const undervisningsgrupper = groups.filter(isUndervisningsgruppe)
   logger('info', ['utils', 'extract-groups-undervisningsgrupper', 'undervisningsgrupper', undervisningsgrupper.length])
-  await writeFile('data/undervisningsgrupper.json', JSON.stringify(undervisningsgrupper, null, 2), 'utf-8')
+  await writeFile('data/undervisningsgrupper.json', JSON.stringify(undervisningsgrupper.map(repackGroup), null, 2), 'utf-8')
   logger('info', ['utils', 'extract-groups-undervisningsgrupper', 'finished'])
 })()
