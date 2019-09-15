47e9c6c7a21e2bed23aaa6dbfa156d82c56a283e
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-15 07:11:48 +0200

    Implements repackGroup

--- utils/extract-groups-basisgrupper.js
+++ utils/extract-groups-basisgrupper.js
@@ -2,10 +2,11 @@
   const { writeFile } = require('fs').promises
   const logger = require('../lib/logger')
   const isBasisgruppe = require('../lib/is-basisgruppe')
+  const repackGroup = require('../lib/repack-group')
   const groups = require('../data/groups.json')
   logger('info', ['utils', 'extract-groups-basisgrupper', 'groups', groups.length])
   const basisgrupper = groups.filter(isBasisgruppe)
   logger('info', ['utils', 'extract-groups-basisgrupper', 'basisgrupper', basisgrupper.length])
-  await writeFile('data/basisgrupper.json', JSON.stringify(basisgrupper, null, 2), 'utf-8')
+  await writeFile('data/basisgrupper.json', JSON.stringify(basisgrupper.map(repackGroup), null, 2), 'utf-8')
   logger('info', ['utils', 'extract-groups-basisgrupper', 'finished'])
 })()
