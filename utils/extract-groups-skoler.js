14bbf4eea2abb09c5044403e5b3d5b8d499c5904
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-15 10:41:06 +0200

    Implements repackSchool

--- utils/extract-groups-skoler.js
+++ utils/extract-groups-skoler.js
@@ -2,10 +2,11 @@
   const { writeFile } = require('fs').promises
   const logger = require('../lib/logger')
   const isSkole = require('../lib/is-skole')
+  const repackSchool = require('../lib/repack-school')
   const groups = require('../data/groups.json')
   logger('info', ['utils', 'extract-groups-skoler', 'groups', groups.length])
   const skoler = groups.filter(isSkole)
   logger('info', ['utils', 'extract-groups-skoler', 'skoler', skoler.length])
-  await writeFile('data/skoler.json', JSON.stringify(skoler, null, 2), 'utf-8')
+  await writeFile('data/skoler.json', JSON.stringify(skoler.map(repackSchool), null, 2), 'utf-8')
   logger('info', ['utils', 'extract-groups-skoler', 'finished'])
 })()
