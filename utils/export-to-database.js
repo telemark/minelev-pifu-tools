8e7b27cccabea74014c867e3bd057ccdb3f0967c
Mats Andreassen <matsand@live.no>
2019-09-17 13:19:21 +0200

    Updated database export. Fuck sharded keys..

--- utils/export-to-database.js
+++ utils/export-to-database.js
@@ -8,7 +8,6 @@
   const db = await mongo()
   const dbName = process.env.MONGODB_NAME
   const dbCollection = process.env.MONGODB_COLLECTION
-  const dbTempCollection = `${dbCollection}Temp`
   const data = []
   const skoleeier = require('../data/skoleeier.json')
   const skoler = require('../data/skoler.json')
@@ -18,7 +17,7 @@
   const undervisningsgrupper = require('../data/undervisningsgrupper.json')
   const students = require('../data/students.json')
   const teachers = require('../data/teachers.json')
-  let tempTjommi = db.collection(dbTempCollection)
+  let tjommi = db.collection(dbCollection)
 
   data.push(...skoleeier)
   data.push(...skoler)
@@ -29,36 +28,35 @@
   data.push(...students)
   data.push(...teachers)
 
-  // Create temp collection
+  // Drop existing collection
   try {
-    logger('info', ['lib', 'export-to-database', 'create temp collection'])
-    await db.executeDbAdminCommand( { shardCollection: `${dbName}.${dbTempCollection}`, key: { _id: "hashed" } })
-    tempTjommi = db.collection(dbTempCollection)
+    logger('info', ['lib', 'export-to-database', 'drop collection'])
+    await tjommi.drop()
   } catch (error) {
-    logger('info', ['lib', 'export-to-database', 'create temp collection', 'unable to create temp collection', error])
+    logger('info', ['lib', 'export-to-database', 'drop collection', 'unable to drop', error])
   }
+
+  // Create collection
+  try {
+    logger('info', ['lib', 'export-to-database', 'create collection'])
+    await db.executeDbAdminCommand( { shardCollection: `${dbName}.${dbCollection}`, key: { _id: "hashed" } })
+  } catch (error) {
+    logger('info', ['lib', 'export-to-database', 'create collection', 'unable to create collection', error])
+  }  
+
+  tjommi = db.collection(dbCollection)
 
   // Insert data
   logger('info', ['lib', 'export-to-database', 'data', data.length, 'start'])
   while (data.length > 0) {
     const chunk = data.splice(0, bulkSize)
     logger('info', ['lib', 'export-to-database', 'chunk', chunk.length, 'ready'])
-    const result = await tempTjommi.insertMany(chunk)
+    const result = await tjommi.insertMany(chunk)
     logger('info', ['lib', 'export-to-database', 'chunk', 'inserted', result])
     logger('info', ['lib', 'export-to-database', 'data', data.length, 'remains'])
     await sleep(sleepTime)
   }
-  logger('info', ['lib', 'export-to-database', 'data', 'insert finished'])
-
-
-  // Rename temp collection and drop existing
-  try {
-    logger('info', ['lib', 'export-to-database', 'rename temp collection'])
-    await db.renameCollection(dbTempCollection, dbCollection, { dropTarget: true })
-  } catch (error) {
-    logger('info', ['lib', 'export-to-database', 'rename temp collection', 'unable to rename', error])
-  }
-
+  
   logger('info', ['lib', 'export-to-database', 'finished'])
   process.exit(0)
 })()
