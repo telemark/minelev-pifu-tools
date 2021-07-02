4231c7982e1cc4206843eb1203abb0d0f9e221c0
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-07-02 12:11:08 +0200

    Tools moved to tools

--- utils/tools/get-unique-grouptypes.js
+++ utils/tools/get-unique-grouptypes.js
@@ -1,8 +1,8 @@
 (async () => {
   const groups = require('./data/groups.json')
   const uniques = groups.reduce((accumulator, current) => {
-    if (!accumulator.includes(current.grouptype.typevalue['#text'])) {
-      accumulator.push(current.grouptype.typevalue['#text'])
+    if (!accumulator.includes(current.grouptype.typevalue.text)) {
+      accumulator.push(current.grouptype.typevalue.text)
     }
     return accumulator
   }, [])
--- utils/tools/get-unique-useridtypes.js
+++ utils/tools/get-unique-useridtypes.js
@@ -2,9 +2,9 @@
   const persons = require('./data/persons.json')
   const uniques = persons.reduce((accumulator, current) => {
     if (Array.isArray(current.userid)) {
-      const uniques = current.userid.filter(uid => !accumulator.includes(uid['@_useridtype']))
+      const uniques = current.userid.filter(uid => !accumulator.includes(uid.useridtype))
       if (uniques.length > 0) {
-        const ids = uniques.map(item => item['@_useridtype'])
+        const ids = uniques.map(item => item.useridtype)
         accumulator.push(...ids)
       }
     }
