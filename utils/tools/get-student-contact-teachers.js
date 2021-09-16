485ba2e04cc7dd98cb24dc36374d4a0a70070f6c
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-09-16 15:33:54 +0200

    Added tool to find users contact teachers

---
+++ utils/tools/get-student-contact-teachers.js
@@ -0,0 +1,32 @@
+const { writeFileSync } = require('fs')
+const { join } = require('path')
+const students = require('../../data/students.json')
+const memberships = require('../../data/memberships.json')
+const teachers = require('../../data/teachers.json')
+
+const args = process.argv.slice(2)
+if (args.length === 0) {
+  console.error('Pass student fnr along (node get-student-contact-teachers.js 01234567890)')
+  process.exit(1)
+}
+const studentFnr = args[0]
+
+const student = students.find(stud => stud.id === studentFnr)
+if (!student) {
+  console.error('Student not found')
+  process.exit(1)
+}
+
+student.kontaktlarergruppeIds.forEach(contactGroupId => {
+  const contactGroupMembership = memberships.find(membership => membership.id === contactGroupId)
+  const teacherIds = contactGroupMembership.members.filter(member => member.role === 'teacher').map(teacher => teacher.id)
+  const contactTeachers = teacherIds.map(teach => teachers.filter(t => t.id === teach).map(teacher => ({ id: teacher.id, fullName: teacher.fullName, username: teacher.username })))
+  console.log('Contact teachers in', contactGroupId)
+  console.dir(contactTeachers)
+})
+
+/* if (missingContactTeacher) {
+  const path = join(__dirname, '/../../data/students-missing-contact-teacher.json')
+  writeFileSync(path, JSON.stringify(missingContactTeacher, null, 2), 'utf8')
+  console.log(missingContactTeacher.length, 'students written to', path)
+} */
